40a56d3b229b95d31eae373fc516b90025bb1928
¼­¿µ¿ì <seo_youngwoo02@eland.co.kr>
2023-07-31 16:18:05 +0900

    init

---
+++ src/main/kotlin/com/urizip/korine/KorineApplication.kt
@@ -0,0 +1,11 @@
+package com.urizip.korine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KorineApplication
+
+fun main(args: Array<String>) {
+    runApplication<KorineApplication>(*args)
+}
---
+++ src/test/kotlin/com/urizip/korine/KorineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.urizip.korine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KorineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
