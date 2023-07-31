9840534f533a69893f7fcaf87d02fa44980cc4fc
seo_youngwoo <devsyw@gmail.com>
2023-07-31 17:35:11 +0900

    [feat] kotlin setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.8.22" // 플러그인은 프로젝트에서 사용할 Kotlin 버전을 정의합니다.
+    kotlin("plugin.spring") version "1.8.22" // Kotlin 클래스가 Spring Framework 기능과 호환되도록 하기 위해 수정자를 추가하기 위한 Kotlin Spring 컴파일러 플러그인
     kotlin("plugin.jpa") version "1.8.22"
 }
 
@@ -23,9 +23,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // 이 모듈은 Kotlin 클래스 및 데이터 클래스의 직렬화 및 역직렬화 지원을 추가합니다.
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // 코틀린 리플렉션 라이브러리
     runtimeOnly("com.h2database:h2")
+    testImplementation(kotlin("test")) // test lib
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
